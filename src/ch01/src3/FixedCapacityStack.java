f96ea59dd27de3db776fec969156374871f1f4f0
anggao <ang.gao87@gmail.com>
2014-04-03 09:20:54 +0100

    FixedCapacityStack

---
+++ src/ch01/src3/FixedCapacityStack.java
@@ -0,0 +1,43 @@
+package ch01.src3;
+
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+
+public class FixedCapacityStack<Item> {
+	private Item[] a;
+	private int N;
+
+	public FixedCapacityStack(int cap) {
+		a = (Item[]) new Object[cap];
+	}
+
+	public boolean isEmpty() {
+		return N == 0;
+	}
+
+	public void push(Item item) {
+		a[N++] = item;
+	}
+
+	public Item pop() {
+		return a[--N];
+	}
+
+	public int size() {
+		return N;
+	}
+
+	public static void main(String[] args) {
+		FixedCapacityStack<String> s;
+		s = new FixedCapacityStack<String>(100);
+		while (!StdIn.isEmpty()) {
+			String item = StdIn.readString();
+			if (!item.equals("-"))
+				s.push(item);
+			else if (!s.isEmpty())
+				StdOut.print(s.pop() + " ");
+		}
+
+		StdOut.println("(" + s.size() + " left on stack");
+	}
+}
